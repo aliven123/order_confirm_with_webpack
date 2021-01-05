8205a0ba7df1f74866d3d6c72825f4fbbac72569
aliven123 <2227969247@qq.com>
2021-01-05 16:54:05 +0800

    更新了域名

--- webpack.build.js
+++ webpack.build.js
@@ -53,6 +53,15 @@ module.exports={
 			},{
 				loader:'less-loader'
 			}]
+		},{
+			test: /\.(eot|ttf|svg|woff)$/,
+			use: {
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]',
+					outputPath: 'images/'
+				}
+			}
 		}]
 	},
 	plugins: [
